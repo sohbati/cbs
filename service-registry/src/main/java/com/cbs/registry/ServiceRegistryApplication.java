babf46fa217de0c15b1f8082a530cc81a716de40
r.sohbati <sohbati@bluteam.ir>
2022-08-03 17:04:37 +0430

    init service registry

--- service-registry/src/main/java/com/cbs/registry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/cbs/registry/ServiceRegistryApplication.java
@@ -1,4 +1,4 @@
-package com.example.eurekaserver;
+package com.cbs.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
-    }
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+
 }
