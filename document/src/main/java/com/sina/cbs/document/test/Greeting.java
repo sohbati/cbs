f95986443771c0a2ece2f37a34f5b6c6c73e4106
Reza Sohbati <Sohbati@gmail.com>
2022-11-29 22:00:37 +0330

    Document Project init
    Document

---
+++ document/src/main/java/com/sina/cbs/document/test/Greeting.java
@@ -0,0 +1,19 @@
+package com.sina.cbs.document.test;
+
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
