a59b251dabbf28ed1f63b9981e51a3b90e0ff6e4
Reza Sohbati <Sohbati@gmail.com>
2022-12-03 00:31:02 +0330

    commit all

--- document/target/classes/static/js/context-menu.js
+++ document/target/classes/static/js/context-menu.js
@@ -12,13 +12,28 @@ const contextMenu = {
         } else {
             if (e.target.id != null && e.target.id.includes("component_")) {
                 e.preventDefault();
+                $("#componentContextMenu")[0].rightClickedComponent = e.target.id;
                 const menu = $("#componentContextMenu")[0]
                 menu.style.display = 'block';
                 menu.style.left = e.pageX + "px";
                 menu.style.top = e.pageY + "px";
             }
         }
+    },
+
+    gitInfo: function () {
+        const id = $("#componentContextMenu")[0].rightClickedComponent;
+        const componentName = id.substr(10, id.length);
+        const component = this.getComponentByName(componentName);
+
+        document.open('blank', component.gitInfourl);
+    },
+
+    getComponentByName: function (name) {
+        return componentListManagement.getSelectedComponent(name);
     }
+
+
 }
 
 document.oncontextmenu = contextMenu.rightClick;
